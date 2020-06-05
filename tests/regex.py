0f205a829a8695b46cfd50f5cf2e125e3f7928d1
Ayush Arora <ayusharora009@gmail.com>
2020-06-04 19:51:25 -0700

    Update regex.py

--- tests/regex.py
+++ tests/regex.py
@@ -1,3 +1,4 @@
+# Module Regular Expressions(RE)
 import re
 
 email_pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
@@ -24,3 +25,41 @@ user_input = input()
 
 new_user_input = re.sub(pattern, new_pattern, user_input)
 print(new_user_input)
+
+# ===========================================
+
+# Regular expressions are compiled into pattern objects
+
+#case sensitive
+p = re.compile('[a-e]')
+
+print(p.findall("Aye, said Mr. Gibenson Stark")) 
+# ['e', 'a', 'd', 'b', 'e', 'a']
+
+# ===========================================
+
+# split()
+#  re.split(pattern, string, maxsplit=0, flags=0)
+
+# sub() - substring
+# repl - replace string
+# re.sub(pattern, repl, string, count=0, flags=0)
+print(re.sub('ub', '_test_repace_' , 'Subject has Uber booked already', flags = re.IGNORECASE)) 
+# S_test_repace_ject has _test_repace_er booked already
+
+# ===========================================
+
+# subn()
+
+# It returns a tuple with count of total of replacement
+# and the new string rather than just the string.
+
+# re.subn(pattern, repl, string, count=0, flags=0)
+
+print(re.subn('ub', '~*' , 'Subject has Uber booked already'))
+# ('S~*ject has Uber booked already', 1)
+
+# escape()
+# re.escape(string)
+print(re.escape("This is Awseome even 1 AM")) 
+# This\ is\ Awseome\ even\ 1\ AM
