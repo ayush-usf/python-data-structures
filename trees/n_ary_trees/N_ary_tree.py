8a0be3825089e37b011aa4c9930a0e73bb49cb1b
ayush884 <ayush@whatsbusy.com>
2020-06-10 23:32:54 -0700

    minor

--- trees/n_ary_trees/N_ary_tree.py
+++ trees/n_ary_trees/N_ary_tree.py
@@ -1,3 +1,5 @@
+# https://github.com/joaohelis/n_ary_tree_with_python/blob/master/n_ary_tree.py
+
 from trees.n_ary_trees.Node import Node
 
 class NAryTree(Node):
@@ -5,3 +7,6 @@ class NAryTree(Node):
     def __init__(self):
         self.root = None
         self.size = 0
+    
+    def insert(self, key, parent_key):
+        pass
