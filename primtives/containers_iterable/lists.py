2928ef766fa042c7956f26d77bda127b3dc33c7a
ayush884 <ayush@whatsbusy.com>
2020-07-13 13:31:52 -0700

    sorted with second word

--- primtives/containers_iterable/lists.py
+++ primtives/containers_iterable/lists.py
@@ -58,7 +58,7 @@ class_list.append(Student("Adarsh",2))
 class_list.append(Student("Ameya",3))
 
 # This will print list of objects
-print(\n'class_list: ', class_list)
+print('\nclass_list: ', class_list)
 
 for obj in class_list:
     print(obj,"|", obj.name,"|", obj.roll)
@@ -85,8 +85,17 @@ print("\nSorted list (new iterable):", sorted(logs))
 print("Reverse sorted list (new iterable):", sorted(logs, reverse = True))
 print("Original list after sorting:", logs,"\n") 
 
+#  The logs should be ordered lexicographically ignoring identifier (word 1), 
+#  with the identifier used in case of ties.
+logs  = ["let2 own kit dig","let2 art zero","let1 art can"]
+
+print('logs: ', logs)
+print('sorted(logs, key = lambda x : x.split(" ")[1]): ', sorted(logs, key = lambda x : x.split(" ")[1]))
+print('sorted(logs, key = lambda x : x.split(" ")[1:]): ', sorted(logs, key = lambda x : x.split(" ")[1:]),"\n")
 
 print("========== sorting - sort() [Uses Timsort]===============")
+# sort() is a method of list class and can only be used with lists.
+
 # Timsort
 #       hybrid stable sorting algorithm, derived from merge sort and insertion sort
 
@@ -96,3 +105,10 @@ print("========== sorting - sort() [Uses Timsort]===============")
 #       Average case performance     O(nlogn)
 #       Worst case space complexity - O(n)
 #          Timsort can require a temp array containing as many as N//2 pointers
+
+def sortSecond(val):  
+    return val[1]
+
+list1 = [(1, 2), (3, 3), (1, 1)]
+list1.sort(key = sortSecond)
+print('list1.sort(): ', list1)
